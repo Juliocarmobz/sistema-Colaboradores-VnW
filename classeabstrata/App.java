e0b3c6465c3533c37352e31b973ae2d3291ebfc0
Juliobz <bzjuliocarmo@hotmail.com>
2024-01-26 11:08:39 -0300

    docs: removendo documentacao duplicada

--- classeabstrata/App.java
+++ classeabstrata/App.java
@@ -9,14 +9,14 @@ public class App {
 		devJunior.aumentarSalario();
 		System.out.println(devJunior.vizualizar());
 
-//		devJunior.desvincularColaborador();
-//		System.out.println(devJunior.vizualizar());
+		devJunior.desvincularColaborador();
+		System.out.println(devJunior.vizualizar());
 
 		devJunior.tecnologias();
 
-		DevFrontEnd devSenior = new DevFrontEnd("Julio", "5512-1", 1000.2, "java");
-		System.out.println(devSenior.vizualizar());
-		devSenior.tecnologias();
+//		DevFrontEnd devSenior = new DevFrontEnd("Julio", "5512-1", 1000.2, "java");
+//		System.out.println(devSenior.vizualizar());
+//		devSenior.tecnologias();
 	}
 
 }
